513d91502a32261e80a3baf67b97dc5a9a19f7fd
Jessie Keck <jessie.keck@gmail.com>
2019-12-17 16:35:26 -0800

    Fix up javascript

--- .github/actions/hello-world/hello-world.js
+++ .github/actions/hello-world/hello-world.js
@@ -3,18 +3,18 @@ const github = require('@actions/github');
 
 try {
   const token = process.env['GITHUB_TOKEN'];
-  if (!token) return
+  if (!token) return;
 
   // Create the octokit client
-  const octokit: github.GitHub = new github.GitHub(token)
-  const nwo = process.env['GITHUB_REPOSITORY'] || '/'
-  const [owner, repo] = nwo.split('/')
+  const octokit = new github.GitHub(token);
+  const nwo = process.env['GITHUB_REPOSITORY'] || '/';
+  const [owner, repo] = nwo.split('/');
   console.log(`Owner: ${owner}`);
   console.log(`Repo: ${repo}`);
 
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
